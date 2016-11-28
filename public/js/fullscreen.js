b9d8ea6bd6c51f5216a41e83184b0f64ac8fa1f2
Niklas Hagner <niklas.hagner@outlook.com>
2016-11-28 19:48:21 +0100

    browser fullscreen handling

--- public/js/fullscreen.js
+++ public/js/fullscreen.js
@@ -1,23 +1,35 @@
 var isFullscreen = false;
-$('.fullscreen-toggle').on('click', function() {
-    var icon = $(".fullscreen-toggle i");
-    //var isFullscreen = player.isFullscreen();
+$('.js-fullscreen-toggle').on('click', function() {
+    toggleFullscreen('.js-fullscreen-toggle', false, 'fa fa-arrow-up', 'fa fa-arrow-down');
+});
 
+var isWindowFullscreen = false;
+$('.fullscreen-toggle-2').on('click', function() {
+    var useWindowFullscreen = !isWindowFullscreen;
+    toggleFullscreen('.fullscreen-toggle-2', useWindowFullscreen, 'fa fa-arrows-alt', 'fa fa-times');
+    if (!useWindowFullscreen) 
+        exitFullscreenBrowser();
+    isWindowFullscreen = useWindowFullscreen;
+});
+
+function toggleFullscreen(selector, requestBrowserFullscreen, collapsedIconClass, expandedIconClass) {
+    var icon = $(selector + " i");
     $(".fullscreen-menu").toggleClass("hidden");;
     $("#tv").toggleClass("fullscreen");
     $("#content").toggleClass("fullscreen");
 
     if(isFullscreen){
         isFullscreen = false;
-        //player.closeFullscreen();
-        icon.attr('class','fa fa-arrows-alt');
+        icon.attr('class', collapsedIconClass);
     }
     else{
         isFullscreen = true;
-        //player.openFullscreen();
-        icon.attr('class','fa fa-times');
+        icon.attr('class', expandedIconClass);
+        if (requestBrowserFullscreen) {
+            enterFullscreenBrowser(document.body);
+        }
     }
-});
+}
 
 var mousemovetimer =  null;
 var showingFullscreenMenu = false;
@@ -36,8 +48,26 @@ $('body').mousemove(function() {
          console.log("hiding fs menu");
     }, 1500);
 });
-// $('body').mousestop(function() {
-//     isMouseIntervalSet = false;
-//     window.clearTimeout(mousemovetimer);
-//     mousemovetimer = null;
-// });
+
+function enterFullscreenBrowser(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+function exitFullscreenBrowser() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
